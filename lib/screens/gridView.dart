cc53e0266aaac132333fa5395339a81af7846e07
dominant001 <43893023+dominant001@users.noreply.github.com>
2020-05-21 02:29:24 +0530

    Update gridView.dart

--- lib/screens/gridView.dart
+++ lib/screens/gridView.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:Benika/screens/GridExample.dart';
 
 class GridExample extends StatefulWidget {
   @override
